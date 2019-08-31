1ec240326e7bb14b19e8929e8e92c04190c11f37
Ryan S <planetlucid@gmail.com>
2019-08-31 20:09:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,23 @@
 # code here!
 
+
 class School
-  attr_accessor :author, :add_student, :grade
+  attr_accessor :author, :page_count  # remove the attr_accessor for genre
+  attr_reader :title, :genre  # add an attr_reader for genre
  
-  def initialize(roster)
-    @roster = roster
-  end
+  GENRES = []
  
-  def roster
-    @roster
+  def initialize(title)
+    @title = title
   end
  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
  
+  # create the writer for genre and add the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+  end
 end
