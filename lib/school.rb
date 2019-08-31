8aa0f8238ea6f2466caf22af3a250b882d067337
Ryan S <planetlucid@gmail.com>
2019-08-31 20:16:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,23 +1,9 @@
-# code here!
-
-
-class School
-  attr_accessor :author, :grade  # remove the attr_accessor for genre
-  attr_reader :title, :genre  # add an attr_reader for genre
- 
-  @roster = {}
- 
-  def initialize(title)
-    @title = title
-  end
- 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
- 
-  # create the writer for genre and add the logic for the class constant
-  def genre=(genre)
-    @genre = genre
-    GENRES << genre 
-  end
-end
+def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
+end 
+  
+end 
